99dd8b8fc6595e8d47fbd5cdc85b590d2a70ce9e
NovikovRomanV <rimm19845@gmail.com>
2020-05-08 23:29:27 +0300

    creat folder components and insert components

---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from "./MyPosts.module.css"
+import Post from "./Post/Post"
+
+const MyPosts = () => {
+    return (
+        <div>
+            My posts
+            <Post message = "Hi, how are you?" likesCount = {15}/>
+            <Post message = "It's my first post." likesCount={20} />
+        </div>
+    )
+};
+
+export default MyPosts;
