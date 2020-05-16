5770dcce1e3428a0bd6e30e8dadb31a30560eba5
NovikovRomanV <rimm19845@gmail.com>
2020-05-16 14:32:45 +0300

    map Dialogs and MyPosts

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,60 @@
 import React from "react";
-import s from "./Dialogs.module.css"
+import s from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
+type DialogsType = {
+    name: string
+    id: number
+};
+
+const DialogsItems = (props: DialogsType) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+};
+
+type MessageType = {
+    message: string
+    id: number
+};
+
+const Message = (props: MessageType) => {
+    return (
+        <div className={s.dialog}>{props.message}</div>
+    )
+};
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Vas"},
+        {id: 3, name: "Bobs"},
+        {id: 4, name: "Alex"},
+        {id: 5, name: "Lex"}
+    ];
+
+    let dialogsElements = dialogsData.map(d => <DialogsItems name={d.name} id={d.id} /> );
+
+    let messagesData = [
+        {id: 1, message: "Hi!"},
+        {id: 2, message: "How is your it-kamasutra?"},
+        {id: 3, message: "Good!"}
+    ];
+
+    let messageElements = messagesData.map(m => <Message message={m.message} id={m.id} /> );
+
     return (
-        <div >
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={s.messages}>
+                {messageElements}
+            </div>
         </div>
     )
 };
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import s from "./MyPosts.module.css"
 import Post from "./Post/Post"
 
+
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: "Hi, how are you?", likesCount: 15},
+        {id: 2, message: "It's my first post.", likesCount: 20}
+    ];
+
+    let postElement = postData.map( p => <Post message={p.message} likesCount={p.likesCount} id={p.id}/> );
+
     return (
-        <div>
-            My posts
-            <Post message = "Hi, how are you?" likesCount = {15}/>
-            <Post message = "It's my first post." likesCount={20} />
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div>
+                <div>
+                    <textarea ></textarea>
+                </div>
+                <button>Add post</button>
+            </div>
+            <div className={s.posts}>
+                {postElement}
+            </div>
         </div>
     )
 };
